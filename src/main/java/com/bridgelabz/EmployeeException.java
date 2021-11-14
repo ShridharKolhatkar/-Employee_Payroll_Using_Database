3fff734ad5d5605cea837f68224b018ed39d83bb
shridhar <beaprogrammer24@gmail.com>
2021-11-14 22:54:46 +0530

    Added: Creating a database in mysql Shell and connect it

---
+++ src/main/java/com/bridgelabz/EmployeeException.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+/**
+ * Purpose: This class is used for creting Costume Exception
+ * @author SHRIDHAR KOLHATKAR
+ *
+ */
+public class EmployeeException extends RuntimeException{
+
+    public EmployeeException(String message) {
+        super(message);
+    }
+}
+
+
+
